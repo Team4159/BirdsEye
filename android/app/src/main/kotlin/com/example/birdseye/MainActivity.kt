f75c68dc358bf295d3c77b27aef358a8e38642e7
Max L <maliu2@s.sfusd.edu>
2025-07-29 16:56:51 -0700

    deduplicate android build files, fix linux build files

--- android/app/src/main/kotlin/com/example/birdseye/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package org.team4159.birdseye
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/org/team4159/scouting/birdseye/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package org.team4159.scouting.birdseye
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
